b4b5a6211528ef5ab639fcd8c64e1caa61b4c5e3
baczek113 <baczek.jakub@gmail.com>
2025-05-14 09:44:10 +0200

    :PPP

--- src/ClientModel/ClientModelManager.java
+++ src/ClientModel/ClientModelManager.java
@@ -1,32 +1,24 @@
 package ClientModel;
 
 import ClientModel.ServerInteractions.*;
-import Model.Employee;
-import Model.Project;
-import Model.Sprint;
-import Model.Task;
+import Model.*;
 import Network.Response.Response;
 
 import java.beans.PropertyChangeSupport;
 import java.sql.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ClientModelManager {
     private PropertyChangeSupport propertyChangeSupport;
     private List<Employee> employees;
     private List<Project> projects;
-    private List<Sprint> sprints;
-    private List<Task> tasks;
     private Employee loggedEmployee;
 
     public ClientModelManager()
     {
         propertyChangeSupport = new PropertyChangeSupport(this);
-        employees = new ArrayList<>();
-        projects = new ArrayList<>();
-        sprints = new ArrayList<>();
-        tasks = new ArrayList<>();
+        employees = new EmployeeList();
+        projects = new ProjectList();
         loggedEmployee = null;
     }
 
--- src/Network/ServerModelManager.java
+++ src/Network/ServerModelManager.java
@@ -258,7 +258,7 @@ public class ServerModelManager {
         return false;
     }
 
-
+    
 
     public void setRequestHandler(RequestHandlerStrategy requestHandler) {
         this.requestHandler = requestHandler;
